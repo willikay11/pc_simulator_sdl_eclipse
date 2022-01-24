b1a66ba7171cf9a0b599d8185a344003d5fd923b
willikay11 <willikay11@gmail.com>
2022-01-24 06:37:06 +0300

    Added button matrix

--- lv_widgets/grouped_widgets.c
+++ lv_widgets/grouped_widgets.c
@@ -12,7 +12,8 @@
 void lists(void);
 void groups(void)
 {
-	lists();
+//	lists();
+	btn_matrix();
 }
 
 void list_btn_clicked(lv_obj_t * btn, lv_event_t event)
@@ -23,7 +24,7 @@ void list_btn_clicked(lv_obj_t * btn, lv_event_t event)
 	}
 }
 
-void lists()
+void lists(void)
 {
 	lv_obj_t * list = lv_list_create(lv_scr_act(), NULL);
 	lv_obj_set_size(list, lv_obj_get_width_fit(lv_scr_act()), lv_obj_get_height_fit(lv_scr_act()));
@@ -32,4 +33,29 @@ void lists()
 	lv_obj_set_event_cb(btn, list_btn_clicked);
 	btn = lv_list_add_btn(list, LV_SYMBOL_BELL, "Bell");
 	lv_obj_set_event_cb(btn, list_btn_clicked);
+}
+
+static const char * btn_map[] = {"1", "2", "3", "\n",
+		"4", "5", "6", "\n",
+		"7", "8", "9", "\n",
+		"0", ".", "\n",
+		"+", "-", "*", "/", "\n"
+		"=", ""
+};
+
+void on_btnm_cb(lv_obj_t * btn, lv_event_t event){
+	if(event == LV_EVENT_VALUE_CHANGED) {
+		printf("%s was pressed \n", lv_btnmatrix_get_active_btn_text(btn));
+		fflush(NULL);
+	}
+}
+
+void btn_matrix(void)
+{
+	lv_obj_t * btnm = lv_btnmatrix_create(lv_scr_act(), NULL);
+	lv_btnmatrix_set_map(btnm, btn_map);
+	lv_obj_set_height(btnm, 300);
+	lv_btnmatrix_set_btn_width(btnm, 9, 2);
+	lv_btnmatrix_set_btn_ctrl(btnm, 15, LV_BTNMATRIX_CTRL_CHECK_STATE);
+	lv_obj_set_event_cb(btnm, on_btnm_cb);
 }
